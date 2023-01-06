56d888884185784b7c52c7d9602cbc08097c63c9
hyelim-JEON <hyelimiam@gmail.com>
2023-01-06 22:53:29 +0900

    Feat:logout

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,37 @@
 import express from "express";
 import morgan from "morgan";
+import { AppDataSource } from "./data-source";
+import authRoutes from "./routes/auth";
+import subRoutes from "./routes/subs";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 const app = express();
+const origin = "http://localhost:3000";
+app.use(
+  cors({
+    origin,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
+
+dotenv.config();
 
 app.get("/", (_, res) => res.send("running"));
+app.use("/api/auth", authRoutes);
+app.use("/api/subs", subRoutes);
 
 let port = 4000;
 app.listen(port, async () => {
   console.log(`server running at http://localhost:${port}`);
+
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log("data init");
+    })
+    .catch((error) => console.log(error));
 });
